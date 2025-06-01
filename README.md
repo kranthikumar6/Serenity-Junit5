b5a26e875e56ec17a72237789a58f14326bb52ea
Kranthi Kumar <kranthikumar3799@gmail.com>
2025-06-01 17:37:49 +0530

    Serenity Junit5

---
+++ README.md
@@ -0,0 +1,12 @@
+SERENITY JUNIT5
+
+# Maven Command to Execute test cases
+- mvn clean verify
+
+# Framework Setup:
+- Java 17
+- Intellij
+- Maven
+
+# Test Output:
+- target/site/serenity/index.html
---
+++ src/test/java/todomvc/WhenFilteringTasks.java
@@ -0,0 +1,54 @@
+package todomvc;
+
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static net.serenitybdd.core.Serenity.reportThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+public class WhenFilteringTasks {
+
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+    @Steps
+    TodoListActions todoList;
+
+    @ParameterizedTest(name = "Show correctly filtered items: {0}")
+    @CsvSource({
+            "Active,        Feed the cat;Walk the dog,  Feed the cat, Walk the dog",
+            "Completed,     Feed the cat;Walk the dog,  Feed the cat, Feed the cat",
+            "All,           Feed the cat;Walk the dog,  Feed the cat, Feed the cat;Walk the dog",
+    })
+    public void shouldShowCorrectlyFilteredItems(String filterBy,
+                                                 String todoItems,
+                                                 String completeItem,
+                                                 String filteredItems) {
+
+        todoList.openApplication();
+
+        todoList.addItems(listFrom(todoItems));
+
+        todoList.completeItem(completeItem);
+
+        todoList.filterBy(filterBy);
+
+        reportThat("The todo list should contain the expected items",
+                () -> assertThat(todoList.items()).hasSameElementsAs(listFrom(filteredItems))
+        );
+    }
+
+    private List<String> listFrom(String items) {
+        return stream(items.split(";")).collect(Collectors.toList());
+    }
+}
